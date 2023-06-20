9147eb072f1f47545cc1ade35e5fd560b78153fd
Rigel Ramadhani Waloni <rigel8911@gmail.com>
2023-06-20 11:24:21 +0700

    Update getProduct and saveProduct

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,16 +2,76 @@ const Product = require('../models/Product');
 
 
 // Get all products
-const getProducts = async (req, res) => {};
+const getProducts = async (req, res) => {
+  try{
+    const products = await Product.find();
+    res.status(200).json({
+      // data: users
+      success: true,
+      message: "Get products success",
+      data: products
+    })
+  }
+  catch(error){ // Exception
+    res.status(500).json({
+      message: "Get products failed",
+      data: error
+    })
+  }
+};
 
 // Save a product
-const saveProduct = async (req, res) => {};
+const saveProduct = async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    const savedProduct = await Product.save();
+
+    res.status(201).json({
+      message: "Save product success",
+      data: savedProduct
+    })
+  } catch (error) {
+    res.status(500).json({
+      messgae: "Save product failed",
+      data: error
+    })
+  }
+};
 
 // Update a product
-const updateProduct = async (req, res) => {};
+const updateProduct = async (req, res) => {
+  const productId = req.params.id
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(productId, req.body, {
+      new: true
+    })
+
+    res.status(200).json({
+      message: "Update product success",
+      data: updatedProduct
+    })
+  } catch(error){
+    res.status(500).json({
+      message: "Update product failed",
+      data: error
+    })
+  }
+};
 
 // Delete a product
-const deleteProduct = async (req, res) => {};
+const deleteProduct = async (req, res) => {
+  const productId = req.params.id
+
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(productId, req.body,)
+
+    res.status(200).json({
+      message: "Delete product success",
+    })
+  } catch(error) {
+
+  }
+};
 
 module.exports = {
   getProducts,
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,8 +1,41 @@
 const User = require('../models/User');
 
-const getUsers = async (req, res) => {};
+const getUsers = async (req, res) => {
+  // mongoose
+  // res (dikirim ke Client)
+  try{
+    const users = await User.find();
+    res.status(200).json({
+      // data: users
+      success: true,
+      message: "Get users success",
+      data: users
+    })
+  }
+  catch(error){ // Exception
+    res.status(500).json({
+      message: "Get users failed",
+      data: error
+    })
+  }
+};
 
-const saveUser = async (req, res) => {};
+const saveUser = async (req, res) => {
+  try {
+    const user = new User(req.body);
+    const savedUser = await user.save();
+
+    res.status(201).json({
+      message: "Save user success",
+      data: savedUser
+    })
+  } catch (error) {
+    res.status(500).json({
+      messgae: "Save user failed",
+      data: error
+    })
+  }
+};
 
 module.exports = {
   getUsers,
